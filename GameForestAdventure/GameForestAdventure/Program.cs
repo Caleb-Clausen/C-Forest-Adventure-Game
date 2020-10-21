9b862d2e7463da3eb05f33ec7decae36277b135b
Caleb Clausen <Caleb.Clausen@outlook.com>
2020-10-22 00:14:46 +0200

    Updated the main loop to support scene capabilities

--- GameForestAdventure/GameForestAdventure/Program.cs
+++ GameForestAdventure/GameForestAdventure/Program.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿// This program genertates an active console based game that allows player movment accross the consolse on a 2d array string based grid
+// The player can select differnt options to interact with the world around him through the consele window
+// The end objective for the player is to kill all monsters on the 2d map and return home to his house to feed his family 
+// Main actions are movement, skill usage, saving and loading, grids.
+//
+//
+using System;
 using GameForestAdventure.MenuObjects;
 using GameForestAdventure.Player_And_Monsters;
 using System.Data.SqlClient;
 using GameForestAdventure.MenuObjects.DataHelper;
 using System.IO;
 using System.Linq;
-
 namespace GameForestAdventure
 {
     class Program
@@ -17,17 +22,22 @@ namespace GameForestAdventure
 
         // enum to allow skills of the player and enemies
         public enum CombatActions { Attack, Spellattack, Punch, Clawattack}
-        // enum to allow movement of the player
-        public enum Movement { Front, Left, Right, Back}
+        public enum CurrentScene { Town, Forest, Menu}
+        CurrentScene currentSceneWorld = new CurrentScene();
         static void Main(string[] args)
         {
+            CurrentScene currentSceneWorld = new CurrentScene();
+            //Create a new map and call the SetMapState to create the 2d array from the text file in current working directory
             Map forestMap = new Map();
             forestMap.SetMapState();
-
+            // Bool to check the game state
             bool gameRunning = true;
-
+            // Create a new menu to be called and prompt the user if he wishes to load/play/exit application
             Menu newMenu = new Menu();
             newMenu.StartMenu();
+            currentSceneWorld = CurrentScene.Town;
+            //Create the objects that will be displayed by the forestMap.totalMap[] field
+            // PlayerCharacter is the user, Monster, the NPC class 
             PlayerCharacter player1 = new PlayerCharacter(newMenu.Playername, 10, 20);
             Monster wolf = new Monster("Wolf", 10, 20,forestMap);
             Monster bear = new Monster("bear", 10, 20, forestMap);
@@ -39,9 +49,20 @@ namespace GameForestAdventure
             forestMap.totalMap[player1.ReturnPos().X, player1.ReturnPos().Y] = "p";
             while (gameRunning == true)
             {
+                switch (currentSceneWorld)
+                {
+                    case CurrentScene.Forest:
+                        return;
+                    case CurrentScene.Menu:
+                        return;
+                    case CurrentScene.Town:
+                        forestMap.DisplayMap();
+                        forestMap.PlayerMovement(player1, forestMap);
+                        return;
 
-                forestMap.DisplayMap();
-                forestMap.PlayerMovement(player1, forestMap);
+                }
+
+            
                 //SceneTown.ScenceOne();
             }
             
